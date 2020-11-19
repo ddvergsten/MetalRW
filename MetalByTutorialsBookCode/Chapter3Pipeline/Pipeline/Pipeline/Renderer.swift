a15a9aec1f6a5e8b20117cfbe0a389388576b372
David Dvergsten <ddvergsten@gmail.com>
2020-11-19 11:04:36 -0600

    use train model instead of cube and comment code

--- MetalByTutorialsBookCode/Chapter3Pipeline/Pipeline/Pipeline/Renderer.swift
+++ MetalByTutorialsBookCode/Chapter3Pipeline/Pipeline/Pipeline/Renderer.swift
@@ -12,7 +12,8 @@ class Renderer: NSObject {
     var timer: Float = 0
     static var device: MTLDevice!
     static var commandQueue: MTLCommandQueue!
-    var mesh: MTKMesh!
+    var meshCube: MTKMesh!
+    var meshTrain: MTKMesh!
     var vertexBuffer: MTLBuffer!
     var pipelineState: MTLRenderPipelineState!
     
@@ -27,25 +28,48 @@ class Renderer: NSObject {
         metalView.device = device
         
         //todo: load mesh from train.obj this time
+        let allocator = MTKMeshBufferAllocator(device: device)
+        guard let assetURL = Bundle.main.url(forResource: "train", withExtension: "obj") else {
+            fatalError()
+        }
+        let vertexDescriptor2 = MTLVertexDescriptor()
+        vertexDescriptor2.attributes[0].format = .float3
+        vertexDescriptor2.attributes[0].offset = 0
+        vertexDescriptor2.attributes[0].bufferIndex = 0
+        vertexDescriptor2.layouts[0].stride = MemoryLayout<SIMD3<Float>>.stride
+        
+        let meshDescriptor = MTKModelIOVertexDescriptorFromMetal(vertexDescriptor2)
+        (meshDescriptor.attributes[0] as! MDLVertexAttribute).name = MDLVertexAttributePosition
+        let asset = MDLAsset(url: assetURL, vertexDescriptor: meshDescriptor, bufferAllocator: allocator)
+        let mdlMesh2 = asset.childObjects(of: MDLMesh.self).first as! MDLMesh
+        do{
+            meshTrain = try MTKMesh(mesh: mdlMesh2, device: device)
+        }
+        catch let error{
+            print(error.localizedDescription)
+        }
         let mdlMesh = Primitive.makeCube(device: device, size: 1)
         do{
             //we need a mtkmesh so we can create the vertexbuffer for the pipeline
-            mesh = try MTKMesh(mesh: mdlMesh, device: device)
+            meshCube = try MTKMesh(mesh: mdlMesh, device: device)
         }catch let error{
             print(error.localizedDescription)
         }
-        vertexBuffer = mesh.vertexBuffers[0].buffer
+        
+        //for use later, this points to the buffer containing the vertices for the train
+        vertexBuffer = meshTrain.vertexBuffers[0].buffer
+        
+        //initilize our shader code into a library ie like opengl program
         let library = device.makeDefaultLibrary()
         let vertexFunction = library?.makeFunction(name: "vertex_main")
         let fragmentFunction = library?.makeFunction(name: "fragment_main")
         
+        //setup the pipeline so it knows how to act on the data we give it
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
         pipelineDescriptor.vertexFunction = vertexFunction
         pipelineDescriptor.fragmentFunction = fragmentFunction
-        
-        //todo: we might need to now setup our own vertexdescriptor saying the stride of the buffer
-        //  and which portion is the position data etc
-        pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlMesh.vertexDescriptor)
+    
+        pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlMesh2.vertexDescriptor)
         pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
         
         do{
@@ -76,10 +100,19 @@ extension Renderer: MTKViewDelegate {
         //drawing code goes here
         timer += 0.05
         var currentTime = sin(timer)
+        
+        //set value in shader (timer) which is at index 1 right after the position data?
         renderEncoder.setVertexBytes(&currentTime, length: MemoryLayout<Float>.stride, index: 1)
+        
+        //make this pipeline state current, ie like setting program/vertex stride ie index = 0 stride = 3 floats for xyz
         renderEncoder.setRenderPipelineState(pipelineState)
+        
+        //set the buffer with the actual data, ie vertices of the train model
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        for submesh in mesh.submeshes {
+        for submesh in meshTrain.submeshes {
+            
+            //draw the train model ie like opengl drawelements command, except here I think we can stack up all our
+            //draw commands and make one call to commandbuffer.present/commit
             renderEncoder.drawIndexedPrimitives(type: .triangle, indexCount: submesh.indexCount, indexType: submesh.indexType, indexBuffer: submesh.indexBuffer.buffer, indexBufferOffset: submesh.indexBuffer.offset)
         }
         
